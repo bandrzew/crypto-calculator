fdf366fdc58fb7d8fb87318db64b10200f33c06d
bandrzew <bandrzew@gmail.com>
2022-11-26 16:27:36 +0100

    Initial commit

---
+++ src/main/java/com/crypto/calculator/CryptoCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/calculator/CryptoCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
