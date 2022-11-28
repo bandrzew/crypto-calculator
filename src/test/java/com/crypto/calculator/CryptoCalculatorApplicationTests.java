254fcdac3ecdb4c69055857bc5afd7f5af8fc8aa
bandrzew <bandrzew@gmail.com>
2022-11-28 21:32:25 +0100

    Add unit tests

--- src/test/java/com/crypto/calculator/CryptoCalculatorApplicationTests.java
+++ src/test/java/com/crypto/calculator/CryptoCalculatorApplicationTests.java
@@ -1,13 +1,25 @@
 package com.crypto.calculator;
 
+import com.crypto.calculator.coin.api.service.CoinApiService;
+import com.crypto.calculator.currency.controller.CurrencyController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CryptoCalculatorApplicationTests {
 
+	@Autowired
+	private CoinApiService coinApiService;
+
+	@Autowired
+	private CurrencyController currencyController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(coinApiService).isNotNull();
+		Assertions.assertThat(currencyController).isNotNull();
 	}
 
 }
